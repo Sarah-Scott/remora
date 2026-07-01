c86d6a1dfab947baf1cae31a82d58f84672d2b42
sarahjohnson <sarahjohnson@pop-os.lan>
2026-07-01 15:55:41 -0500

    modify some names

--- src/Serializer.hs
+++ src/Serializer.hs
@@ -16,8 +16,8 @@ import Syntax qualified
 -- Dim
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.Dim T.Text) where
-  toJSON (Syntax.DimVar v) = object ["tag" .= ("DimVar" :: T.Text), "var"  .= v]
-  toJSON (Syntax.DimN   n) = object ["tag" .= ("DimN"   :: T.Text), "n"    .= n]
+  toJSON (Syntax.DimVar v) = object ["tag" .= ("DimVar" :: T.Text), "name" .= v]
+  toJSON (Syntax.DimN   n) = object ["tag" .= ("DimN"   :: T.Text), "val"  .= n]
   toJSON (Syntax.Add  ds)  = object ["tag" .= ("Add"    :: T.Text), "dims" .= ds]
   toJSON (Syntax.Mul  ds)  = object ["tag" .= ("Mul"    :: T.Text), "dims" .= ds]
   toJSON (Syntax.Sub  ds)  = object ["tag" .= ("Sub"    :: T.Text), "dims" .= ds]
@@ -26,7 +26,7 @@ instance ToJSON (Syntax.Dim T.Text) where
 -- Shape
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.Shape T.Text) where
-  toJSON (Syntax.ShapeVar v)    = object ["tag" .= ("ShapeVar" :: T.Text), "var"    .= v]
+  toJSON (Syntax.ShapeVar v)    = object ["tag" .= ("ShapeVar" :: T.Text), "name"   .= v]
   toJSON (Syntax.ShapeDim d)    = object ["tag" .= ("ShapeDim" :: T.Text), "dim"    .= d]
   toJSON (Syntax.Concat  shapes) = object ["tag" .= ("Concat"  :: T.Text), "shapes" .= shapes]
 
@@ -41,22 +41,22 @@ instance ToJSON (Syntax.Extent T.Text) where
 -- ExtentParam
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.ExtentParam T.Text) where
-  toJSON (Syntax.DimParam   v) = object ["tag" .= ("DimParam"   :: T.Text), "var" .= v]
-  toJSON (Syntax.ShapeParam v) = object ["tag" .= ("ShapeParam" :: T.Text), "var" .= v]
+  toJSON (Syntax.DimParam   v) = object ["tag" .= ("DimParam"   :: T.Text), "name" .= v]
+  toJSON (Syntax.ShapeParam v) = object ["tag" .= ("ShapeParam" :: T.Text), "name" .= v]
 
 -- ---------------------------------------------------------------------------
 -- TypeParam
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.TypeParam T.Text) where
-  toJSON (Syntax.AtomTypeParam  v) = object ["tag" .= ("AtomTypeParam"  :: T.Text), "var" .= v]
-  toJSON (Syntax.ArrayTypeParam v) = object ["tag" .= ("ArrayTypeParam" :: T.Text), "var" .= v]
+  toJSON (Syntax.AtomTypeParam  v) = object ["tag" .= ("AtomTypeParam"  :: T.Text), "name" .= v]
+  toJSON (Syntax.ArrayTypeParam v) = object ["tag" .= ("ArrayTypeParam" :: T.Text), "name" .= v]
 
 -- ---------------------------------------------------------------------------
 -- TypeExp
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.TypeExp T.Text) where
-  toJSON (Syntax.TEAtomVar  v _)    = object ["tag" .= ("TEAtomVar"  :: T.Text), "var"    .= v]
-  toJSON (Syntax.TEArrayVar v _)    = object ["tag" .= ("TEArrayVar" :: T.Text), "var"    .= v]
+  toJSON (Syntax.TEAtomVar  v _)    = object ["tag" .= ("TEAtomVar"  :: T.Text), "name"    .= v]
+  toJSON (Syntax.TEArrayVar v _)    = object ["tag" .= ("TEArrayVar" :: T.Text), "name"    .= v]
   toJSON (Syntax.TEBool       _)    = object ["tag" .= ("TEBool"     :: T.Text)]
   toJSON (Syntax.TEInt        _)    = object ["tag" .= ("TEInt"      :: T.Text)]
   toJSON (Syntax.TEFloat      _)    = object ["tag" .= ("TEFloat"    :: T.Text)]
@@ -70,23 +70,22 @@ instance ToJSON (Syntax.TypeExp T.Text) where
 -- Base
 -- ---------------------------------------------------------------------------
 instance ToJSON Syntax.Base where
-  toJSON (Syntax.BoolVal b)  = object ["tag" .= ("BoolVal"  :: T.Text), "value" .= b]
-  toJSON (Syntax.IntVal  n)  = object ["tag" .= ("IntVal"   :: T.Text), "value" .= n]
-  toJSON (Syntax.FloatVal f) = object ["tag" .= ("FloatVal" :: T.Text), "value" .= f]
+  toJSON (Syntax.BoolVal b)  = object ["tag" .= ("BoolVal"  :: T.Text), "lit" .= b]
+  toJSON (Syntax.IntVal  n)  = object ["tag" .= ("IntVal"   :: T.Text), "lit" .= n]
+  toJSON (Syntax.FloatVal f) = object ["tag" .= ("FloatVal" :: T.Text), "lit" .= f]
 
 -- ---------------------------------------------------------------------------
 -- Pat
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.Pat Syntax.NoInfo T.Text) where
-  toJSON :: Syntax.Pat Syntax.NoInfo T.Text -> Value
-  toJSON (Syntax.PatId v t Syntax.NoInfo _) = object ["name" .= v, "type" .= t]
+  toJSON (Syntax.PatId v t Syntax.NoInfo _) = object ["tag" .= ("PatId"  :: T.Text), "var" .= v, "type" .= t]
 
 -- ---------------------------------------------------------------------------
 -- Atom
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.Atom Syntax.NoInfo T.Text) where
   toJSON (Syntax.Base b Syntax.NoInfo _) =
-    object ["tag" .= ("Base" :: T.Text), "value" .= b]
+    object ["tag" .= ("Base" :: T.Text), "lit" .= b]
   toJSON (Syntax.Lambda ps body Syntax.NoInfo _) =
     object ["tag" .= ("Lambda"  :: T.Text), "params"  .= ps, "body" .= body]
   toJSON (Syntax.TLambda ps body Syntax.NoInfo _) =
@@ -94,24 +93,24 @@ instance ToJSON (Syntax.Atom Syntax.NoInfo T.Text) where
   toJSON (Syntax.ILambda ps body Syntax.NoInfo _) =
     object ["tag" .= ("ILambda" :: T.Text), "params"  .= ps, "body" .= body]
   toJSON (Syntax.Box extents body t Syntax.NoInfo _) =
-    object ["tag" .= ("Box"     :: T.Text), "extents" .= extents, "body" .= body, "type" .= t]
+    object ["tag" .= ("Box"     :: T.Text), "ispaces" .= extents, "array" .= body, "type" .= t]
 
 -- ---------------------------------------------------------------------------
 -- Bind
 -- ---------------------------------------------------------------------------
 instance ToJSON (Syntax.Bind Syntax.NoInfo T.Text) where
   toJSON (Syntax.BindVal v mt body _) =
-    object ["tag" .= ("BindVal"    :: T.Text), "name" .= v, "type" .= mt, "body" .= body]
+    object ["tag" .= ("BindVal"    :: T.Text), "var" .= v, "type" .= mt, "expr" .= body]
   toJSON (Syntax.BindType tp ty Syntax.NoInfo _) =
-    object ["tag" .= ("BindType"   :: T.Text), "param" .= tp, "type" .= ty]
+    object ["tag" .= ("BindType"   :: T.Text), "var" .= tp, "type" .= ty]
   toJSON (Syntax.BindExtent ep ext _) =
-    object ["tag" .= ("BindExtent" :: T.Text), "param" .= ep, "extent" .= ext]
+    object ["tag" .= ("BindExtent" :: T.Text), "var" .= ep, "ispace" .= ext]
   toJSON (Syntax.BindFun v ps mt body Syntax.NoInfo _) =
-    object ["tag" .= ("BindFun"    :: T.Text), "name" .= v, "params"  .= ps, "type" .= mt, "body" .= body]
+    object ["tag" .= ("BindFun"    :: T.Text), "var" .= v, "params" .= ps, "type" .= mt, "expr" .= body]
   toJSON (Syntax.BindTFun v ps mt body Syntax.NoInfo _) =
-    object ["tag" .= ("BindTFun"   :: T.Text), "name" .= v, "params" .= ps, "type" .= mt, "body" .= body]
+    object ["tag" .= ("BindTFun"   :: T.Text), "var" .= v, "params" .= ps, "type" .= mt, "expr" .= body]
   toJSON (Syntax.BindIFun v ps mt body Syntax.NoInfo _) =
-    object ["tag" .= ("BindIFun"   :: T.Text), "name" .= v, "params" .= ps, "type" .= mt, "body" .= body]
+    object ["tag" .= ("BindIFun"   :: T.Text), "var" .= v, "params" .= ps, "type" .= mt, "expr" .= body]
 
 -- ---------------------------------------------------------------------------
 -- Exp
@@ -120,13 +119,13 @@ instance ToJSON (Syntax.Exp Syntax.NoInfo T.Text) where
   toJSON (Syntax.Var v Syntax.NoInfo _) =
     object ["tag" .= ("Var" :: T.Text), "name" .= v]
   toJSON (Syntax.Array shape as Syntax.NoInfo _) =
-    object ["tag" .= ("Array"      :: T.Text), "shape" .= shape, "elems"    .= as]
+    object ["tag" .= ("Array"      :: T.Text), "dims" .= shape, "atoms"    .= as]
   toJSON (Syntax.EmptyArray shape t Syntax.NoInfo _) =
-    object ["tag" .= ("EmptyArray" :: T.Text), "shape" .= shape, "elemType" .= t]
+    object ["tag" .= ("EmptyArray" :: T.Text), "dims" .= shape, "type" .= t]
   toJSON (Syntax.Frame shape es Syntax.NoInfo _) =
-    object ["tag" .= ("Frame"      :: T.Text), "shape" .= shape, "elems"    .= es]
+    object ["tag" .= ("Frame"      :: T.Text), "dims" .= shape, "exprs"    .= es]
   toJSON (Syntax.EmptyFrame shape t Syntax.NoInfo _) =
-    object ["tag" .= ("EmptyFrame" :: T.Text), "shape" .= shape, "elemType" .= t]
+    object ["tag" .= ("EmptyFrame" :: T.Text), "dims" .= shape, "type" .= t]
   toJSON (Syntax.App f args Syntax.NoInfo _) =
     object ["tag" .= ("App"  :: T.Text), "fun" .= f, "args"    .= args]
   toJSON (Syntax.TApp f ts Syntax.NoInfo _) =
@@ -134,6 +133,6 @@ instance ToJSON (Syntax.Exp Syntax.NoInfo T.Text) where
   toJSON (Syntax.IApp f extents Syntax.NoInfo _) =
     object ["tag" .= ("IApp" :: T.Text), "fun" .= f, "args" .= extents]
   toJSON (Syntax.Unbox eps v src body Syntax.NoInfo _) =
-    object ["tag" .= ("Unbox" :: T.Text), "extents" .= eps, "name" .= v, "target" .= src, "body" .= body]
+    object ["tag" .= ("Unbox" :: T.Text), "ispaces" .= eps, "var" .= v, "target" .= src, "body" .= body]
   toJSON (Syntax.Let binds body Syntax.NoInfo _) =
     object ["tag" .= ("Let"   :: T.Text), "binds" .= binds, "body" .= body]
